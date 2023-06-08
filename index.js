464b06cd9b9b210e11919c02cf4b4f8140072ab5
mawiie <wisdom.ifode@yahoo.com>
2023-06-07 22:39:16 -0700

    Added code and asset files

---
+++ index.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-ca5a0-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const tasksListInDB = ref(database, "tasksList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const tasksListEl = document.getElementById("task-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(tasksListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+onValue(tasksListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearTasksListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToTasksListEl(currentItem)
+        }    
+    } else {
+        tasksListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearTasksListEl() {
+    tasksListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToTasksListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationOfItemInDB = ref(database, `tasksList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    tasksListEl.append(newEl)
+}
